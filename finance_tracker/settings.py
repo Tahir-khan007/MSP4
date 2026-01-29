d16d0abee4d04ac743221fa874d94036eba7f44c
Tahir-khan007 <tahirts2000pk@yahoo.com>
2026-01-29 11:34:54 +0000

    railway added to allowed hosts

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.up.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
